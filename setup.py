aac64a233d724067c6d54a984464b7763eb3b9ad
Teja Tanush <tejatanush47@gmail.com>
2025-02-14 21:05:21 +0530

    structure update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+from typing import List
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='teja tanush',
+    author_email='tejatanush47@gmail.com',
+    install_requires=["scikit-learn","pandas","numpy"],
+    packages=find_packages()
+)
